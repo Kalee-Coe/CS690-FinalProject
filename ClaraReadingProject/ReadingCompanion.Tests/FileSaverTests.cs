e4259ca08c16a9a4f6a9ff9c63eb35eef347b9ae
Kalee-Coe <kaleejunk@gmail.com>
2026-04-19 17:29:12 +0000

    first test added

---
+++ ClaraReadingProject/ReadingCompanion.Tests/FileSaverTests.cs
@@ -0,0 +1,25 @@
+﻿namespace ReadingCompanion.Tests;
+
+using ReadingCompanion;
+
+public class FileSaverTests
+{
+    FileSaver fileSaver;
+    string testFileName;
+
+    public FileSaverTests()
+    {
+        testFileName = "test-file.txt";
+        File.Delete(testFileName);
+        fileSaver = new FileSaver(testFileName);
+    }
+
+    [Fact]
+    public void Test_AppendLine()
+    {
+        fileSaver.AppendLine("Test Line");
+        var lines = File.ReadAllLines(testFileName);
+        Assert.Single(lines);
+        Assert.Equal("Test Line", lines[0]);
+    }
+}
